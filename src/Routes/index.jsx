6271d60b3858256648e84c87bb08dd017a0bca30
Ehsan-Rohanii <ehsanrohani.82@gmail.com>
2026-08-11 09:02:27 +0330

    fix route

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -11,54 +11,52 @@ import Login from "../Pages/Auth/Login";
 import Profile from "../Pages/Profile";
 import Order from "../Pages/Order";
 import Products from "../Pages/Products";
+import NotFound from "../Pages/NotFound";
 
 const router = createBrowserRouter([
-    {
-        path: "/register",
-        element: <Register />
-    },
-    {
-        path: "/login",
-        element: <Login />
-    },
-    {
-        path: "/loginPass",
-        element: <LoginPass />
-    },
-    {
-        path: "/loginOtp",
-        element: <LoginOtp />
-    },
-    {
-        path: "/",
-        element: <Layout />,
-        children: [
-            {
-                index: true ,
-                element: <Home /> ,
-            },
-            {
-                path: "categories",
-                element: <Categories />
-            },
-            {
-                path: "brands",
-                element: <Brands />
-            }
-        ],
-    },
-    {
-        path:"/profile" ,
-        element:<Profile/>
-    },
-    {
-        path:"/order" ,
-        element:<Order/>
-    },
-    {
-        path:"/products" ,
-        element:<Products/>
-    },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/loginPass",
+    element: <LoginPass />,
+  },
+  {
+    path: "/loginOtp",
+    element: <LoginOtp />,
+  },
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement:<NotFound/>,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "categories",
+        element: <Categories />,
+      },
+      {
+        path: "brands",
+        element: <Brands />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/order",
+        element: <Order />,
+      },
+    ],
+  },
 ]);
 
 export default router;
